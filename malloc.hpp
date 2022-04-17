b911158f41b31bcd19efd48e17983c11609fabd2
einav242 <93386470+einav242@users.noreply.github.com>
2022-04-17 16:10:58 +0300

    Add files via upload

---
+++ malloc.hpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct free_block {
+    size_t size;
+    struct free_block* next;
+} free_block;
+
+static free_block free_block_list_head = { 0, 0 };
+static const size_t overhead = sizeof(size_t);
+static const size_t align_to = 16;
+void* new_malloc(size_t size);
+void new_free(void* ptr);
